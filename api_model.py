3b1d62640221b7b9fa27fffeb22cb2508c1ec5ba
Simon LABBE <simon_labbe@hotmail.fr>
2023-01-28 15:10:33 +0100

    Update api_model.py

--- api_model.py
+++ api_model.py
@@ -34,6 +34,6 @@ def predict_Customer(data:customer):
 # 4. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app)
+    uvicorn.run(app, host = '127.0.0.1', port=8000)
     
 #uvicorn api_model:app --reload
